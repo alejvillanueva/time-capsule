e6906a5b1695763b029b37dc1ecf2867b71128a8
fstone <164958630+fstone-codes@users.noreply.github.com>
2025-04-04 12:06:42 -0400

    create variables, mixins and global text styling, add NotFoundPage.tsx and required routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import AddCapsulePage from "./pages/AddCapsulePage/AddCapsulePage";
 import LockedCapsulePage from "./pages/LockedCapsulePage/LockedCapsulePage";
 import UnlockedCapsulePage from "./pages/UnlockedCapsulePage/UnlockedCapsulePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
 	return (
@@ -13,6 +14,8 @@ function App() {
 				<Route path="/add" element={<AddCapsulePage />} />
 				<Route path="/locked/:capsuleId" element={<LockedCapsulePage />} />
 				<Route path="/unlocked/:capsuleId" element={<UnlockedCapsulePage />} />
+				<Route path="/unlocked/:capsuleId" element={<UnlockedCapsulePage />} />
+				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
