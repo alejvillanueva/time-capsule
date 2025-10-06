7fa74460581ccceb8e8d29b7afa16586c34e8381
alejvillanueva <alejyvilla@gmail.com>
2025-10-06 12:20:57 -0400

    added db url for prod

--- server/db/db.ts
+++ server/db/db.ts
@@ -2,12 +2,23 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const pool = new Pool({
-	database: process.env.DB_NAME,
-	host: process.env.DB_HOST,
-	password: process.env.DB_PASSWORD,
-	port: parseInt(process.env.DB_PORT ?? "5432"),
-	user: process.env.DB_USER,
-});
+let pool;
+
+// Production
+if (process.env.DATABASE_URL) {
+	pool = new Pool({
+		connectionString: process.env.DATABASE_URL,
+		ssl: { rejectUnauthorized: false },
+	});
+} else {
+	// Development
+	pool = new Pool({
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		password: process.env.DB_PASSWORD,
+		port: parseInt(process.env.DB_PORT ?? "5432"),
+		user: process.env.DB_USER,
+	});
+}
 
 export default pool;
