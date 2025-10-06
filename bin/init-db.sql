d892a94c5de0f888082f4b4f83c0a1a6dcd7c59a
alejvillanueva <alejyvilla@gmail.com>
2025-10-06 11:27:51 -0400

    updated init sql

--- bin/init-db.sql
+++ bin/init-db.sql
@@ -1,16 +1,5 @@
 CREATE TYPE mediums AS ENUM('text', 'image', 'video');
 
-CREATE TABLE memories (
-	id SERIAL PRIMARY KEY,
-	author TEXT,
-	medium mediums,
-	message TEXT, 
-	url TEXT, 
-	added_on TIMESTAMPTZ,
-	capsule_id INT,
-	FOREIGN KEY (capsule_id) REFERENCES capsules(id) ON DELETE CASCADE
-);
-
 CREATE TABLE capsules (
 	id SERIAL PRIMARY KEY,
 	title TEXT,
@@ -21,4 +10,15 @@ CREATE TABLE capsules (
 	edit_by TIMESTAMPTZ,
 	created_on TIMESTAMPTZ, 
 	updated_on TIMESTAMPTZ
+);
+
+CREATE TABLE memories (
+	id SERIAL PRIMARY KEY,
+	author TEXT,
+	medium mediums,
+	message TEXT, 
+	url TEXT, 
+	added_on TIMESTAMPTZ,
+	capsule_id INT,
+	FOREIGN KEY (capsule_id) REFERENCES capsules(id) ON DELETE CASCADE
 );
